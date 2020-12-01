8a80f4b5cfb98c0f5df0fbc622a9001acc1f3b9d
kitanosa <43183258+kitanosa@users.noreply.github.com>
2020-12-01 17:06:22 +0300

    Create person.h

---
+++ person.h
@@ -0,0 +1,12 @@
+#ifndef __person__
+#define __person__
+
+typedef struct {
+    char name[20];
+    char cpu[20];
+    char platform[20];
+    int video_memory;
+    char os[20];
+} computer;
+
+#endif
